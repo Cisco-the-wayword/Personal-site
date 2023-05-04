fd2cdb63e6dbd9c17f8ba8f547560f909f26ed07
Cisco. A <dexterhack800@gmail.com>
2023-05-04 05:39:15 -0600

    Add new projects

--- script.js
+++ script.js
@@ -40,13 +40,13 @@ const projects = [
     linkSource: 'https://github.com/Cisco-the-wayword/Leaderboard',
   },
   {
-    name: 'Multi-Post Stories Gain+Glory',
-    technologies: ['Ruby on Rails', 'CSS', 'JavaScript', 'html'],
-    featuredImage: 'Images/snapshot.png',
+    name: 'To-do List',
+    technologies: ['CSS', 'JavaScript', 'html', 'webpack'],
+    featuredImage: 'Images/todolist.png',
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    liveLink: '#',
-    linkSource: '#',
+      "This repository contains a To-do list application that helps the user keep track of pending actions and acts as a reminder to get them done.",
+    liveLink: 'https://cisco-the-wayword.github.io/list/',
+    linkSource: 'https://github.com/Cisco-the-wayword/To-do-list',
   },
   {
     name: 'Multi-Post Stories Gain+Glory',
